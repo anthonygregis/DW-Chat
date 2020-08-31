9fed8e2c3bd0181b7e6483b19450a70d3b1a2132
Anthony Gregis <anthony@thedevtron.com>
2020-08-30 23:51:30 -0500

    [Socket.IO] First Test

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
     socket.on('login', (data) => {
         connected = true;
         // Display the welcome message
-        var message = "Welcome to Socket.IO Chat – ";
+        var message = "Welcome to the Dark Web – ";
         log(message, {
             prepend: true
         });
